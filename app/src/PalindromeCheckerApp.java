bab633a6fb365fb19cbd502cd1834b8d5ed01d8b
lk3849-arch <lk3849@srmist.edu.in>
2026-02-25 10:27:46 +0530

    [name] Added UsecaseName

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,8 +1,25 @@
 // version 1.0
-//author Abc
-//useCase 1: Welcome page
+// author Abc
+// useCase 1: Welcome page
+// useCase 2: Print Hardcoded Palindrome Result
+
 public class PalindroneCheckerApp {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker App Management System");
+
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
     }
 }
