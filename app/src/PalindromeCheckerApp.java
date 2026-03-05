c60613be32c7faf86de3ca64c91d33eab287ebac
lk3849-arch <lk3849@srmist.edu.in>
2026-03-05 09:22:59 +0530

    uc3

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,25 +1,35 @@
-// version 1.0
+// version 3.0
 // author Abc
-// useCase 1: Welcome page
-// useCase 2: Print Hardcoded Palindrome Result
+// Use Case 3: Reverse String Based Palindrome Check
 
-public class PalindroneCheckerApp {
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Welcome to Palindrome Checker App");
 
-        String word = "madam";
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from the user
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
         String reversed = "";
 
+        // Reverse the string using for loop
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
+        // Compare original and reversed string
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
         }
+
+        sc.close();
     }
 }
